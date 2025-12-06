19aedb4d5a091a5068ebbaa739617592c75c282c
Erez Dagan <erezdagan5@gmail.com>
2025-12-06 21:26:40 +0200

    Initial Commit

---
+++ EscapeRoomManager/EscapeRoomManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EscapeRoomManagerApp.swift
+//  EscapeRoomManager
+//
+//  Created by Erez Dagan on 06/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct EscapeRoomManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
